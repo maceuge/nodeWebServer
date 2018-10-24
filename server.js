790c880ed4f4fd5299aaa5cbd51c414fffb07b7f
Eugenio Vorontsov <maceuge@icloud.com>
2018-10-24 17:14:00 -0300

    Pequeña pagina web personal por hbs

--- server.js
+++ server.js
@@ -1,21 +1,29 @@
 var express = require('express')
 var app = express()
+var hbs = require('hbs');
+require('./hbs/helpers/helpers');
 
+// Para servir paginas estaticas dentro de la carpeta public
+app.use(express.static(__dirname + '/public'));
+
+// Express hbs
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+// Para servir APIrestfull
 app.get('/', function(req, res) {
-
     let salida = {
-        nombre: 'Euegnio',
-        appellido: 'Vr',
-        edad: 32,
-        casado: false,
-        auto: 'BMW',
-        casa: true,
+        nombre: 'eUgeNIo voRONtsOv',
         url: req.url
     }
+    res.render('home', salida);
+    // res.send(salida);
+})
 
-    res.send(salida);
-
-    // res.send('Hello World')
+app.get('/about', function(req, res) {
+    res.render('about', {
+        imageUrl: 'img/no-img.jpg'
+    });
 })
 
 app.listen(3000, () => {
